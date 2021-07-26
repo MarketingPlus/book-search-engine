0cbd762f22bc3ceb2c59230503ddc1d074dc1e42
MarketingPlus <benmarketingplus@gmail.com>
2021-07-26 20:45:07 +1000

    added some code for the navbar to ensure it works properly with and when you click each button it actually sends you to the correct place

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,32 +7,33 @@ import LoginForm from './LoginForm';
 import Auth from '../utils/auth';
 
 const AppNavbar = () => {
+
   // set modal display state
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <Navbar bg='dark' variant='dark' expand='lg'>
-        <Container fluid>
+      <Navbar className="bg-black bl nav" expand='lg'>
+        <Container fluid >
           <Navbar.Brand as={Link} to='/'>
-            Google Books Search
+            
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar'>
-            <Nav className='ml-auto'>
-              <Nav.Link as={Link} to='/'>
-                Search For Books
+            <Nav className='ml-auto extra'>
+              <Nav.Link as={Link} to='/' className="nav-text">
+              Home
               </Nav.Link>
               {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
                 <>
-                  <Nav.Link as={Link} to='/saved'>
-                    See Your Books
+                  <Nav.Link className='bl nav-text' as={Link} to='/saved'>
+                    Saved Books
                   </Nav.Link>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                  <Nav.Link className='nav-text' onClick={Auth.logout}>Logout</Nav.Link>
                 </>
               ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+                <Nav.Link className='nav-text' onClick={() => setShowModal(true)}>Login</Nav.Link>
               )}
             </Nav>
           </Navbar.Collapse>
@@ -45,15 +46,15 @@ const AppNavbar = () => {
         onHide={() => setShowModal(false)}
         aria-labelledby='signup-modal'>
         {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey='login'>
+        <Tab.Container className='border' defaultActiveKey='login'>
           <Modal.Header closeButton>
-            <Modal.Title id='signup-modal'>
-              <Nav variant='pills'>
-                <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
+            <Modal.Title  id='signup-modal'>
+              <Nav variant=''>
+                <Nav.Item className='model'>
+                  <Nav.Link className='bl' eventKey='login'>Login</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                  <Nav.Link className='bl' eventKey='signup'>Sign Up</Nav.Link>
                 </Nav.Item>
               </Nav>
             </Modal.Title>
